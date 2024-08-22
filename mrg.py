77e1ccbc547bd717a499b806d2040dd6df3faa5e
loicfrance <loicfrance.developer@gmail.com>
2024-08-22 23:47:50 +0200

    [WIP] improve `.mrg` implementation

--- mrg.py
+++ mrg.py
@@ -7,16 +7,22 @@ archive files structure.
 #region                        imports, constants
 ################################################################################
 from __future__ import annotations
+from io import BytesIO
 import os
 import struct
-from typing import BinaryIO, Iterable, Literal
+from typing import BinaryIO, Iterable, Literal, overload
+
+from .lenzu import lenzu_decompress
+from .mzp import MzpImage
+from .cbg import CompressedBG
+from .utils.io import BytesRW, BytesReader, BytesWriter
 
 NAM_ENTRY_LEN = 32
 HED_ENTRY_LEN = 8
 MRG_SECTOR_SIZE = 0x800
 
 #endregion #####################################################################
-#region                                HED
+#region                             HED, NAM
 ################################################################################
 
 def readHED(path: str) :
@@ -41,10 +47,6 @@ def writeHED(path: str, entries:Iterable[tuple[int, int, int]]) :
         for (offset, size_sectors, size_decomp_sectors) in entries :
             file.write(struct.pack("<IHH", offset, size_sectors, size_decomp_sectors))
         file.write(b'\xFF'*16)
-
-#endregion #####################################################################
-#region                                NAM
-################################################################################
 
 def readNAM(path: str) :
     assert os.path.exists(path), f"file '{path}' does not exist"
@@ -77,7 +79,7 @@ def writeNAM(path: str, fileNames: Iterable[str]) :
             f"error while writing to {path}: wrong file size {file.tell()}"
 
 #endregion #####################################################################
-#region                                MRG
+#region                             MRG entry
 ################################################################################
 
 class MrgEntry :
@@ -91,6 +93,7 @@ class MrgEntry :
         self._offset_sectors = offset_sectors
         self._size_comp_sectors = size_comp_sectors
         self._size_decomp_sectors = size_decomp_sectors
+        self._data = None
     
     @property
     def compressed(self) :
@@ -98,13 +101,154 @@ class MrgEntry :
     
     @property
     def data(self) -> bytes :
-        file = self._mrg._mrgFile
-        assert file is not None, "mrg file is closed"
-        pos = file.tell()
-        file.seek(self._offset_sectors * MRG_SECTOR_SIZE)
-        buffer = file.read(self._size_comp_sectors * MRG_SECTOR_SIZE)
-        file.seek(pos)
-        return buffer
+        if self._data is not None :
+            return self._data
+        else :
+            file = self._mrg._mrgFile
+            assert file is not None, "mrg file is closed"
+            pos = file.tell()
+            file.seek(self._offset_sectors * MRG_SECTOR_SIZE)
+            buffer = file.read(self._size_comp_sectors * MRG_SECTOR_SIZE)
+            file.seek(pos)
+            return buffer
+    
+    @data.setter
+    def data(self, buffer: bytes) :
+        self._data = buffer
+        # TODO recalculate size_decomp and size_comp
+    
+    def from_file(self, src: BytesReader|str) :
+        """Read the entry data from the specified file
+        
+        Read all bytes from the source file and store it in the entry.
+
+        :param src: source file path or reader, from which the data will be read
+        """
+        if isinstance(src, str) :
+            if os.path.isdir(os.path.abspath(src)) :
+                assert self._name is not None, "file name not provided"
+                src = os.path.join(src, self._name)
+            with open(src, "rb") as file :
+                self.data = file.read()
+        else :
+            self.data = src.read()
+
+    @overload
+    def to_file(self, dest: str | BytesWriter) -> None :
+        """Write the raw entry data to the specified file
+        
+        :param dest: destination file path or writer
+        """
+    @overload
+    def to_file(self, dest: None = None) -> BytesIO :
+        """Write the rawentry data to a file
+        
+        :return: a file containing a copy of the entry data
+        """
+    def to_file(self, dest: BytesWriter | str | None = None):
+        data = self.data
+        match dest :
+            case None :
+                return BytesIO(data)
+            case str() :
+                os.makedirs(os.path.dirname(dest), exist_ok=True)
+                if os.path.isdir(os.path.abspath(dest)) :
+                    assert self._name is not None, "file name not provided"
+                    dest = os.path.join(dest, self._name)
+                with open(dest, "wb") as file :
+                    file.write(data)
+            case _ :
+                dest.write(data)
+        
+    @overload
+    def extract(self, dest: str | BytesRW, *args, **kwargs) -> None :
+        """Extract the content of the entry to the specified location.
+        
+        If the destination is file path that has the same extension \
+        as the entry, the raw data is inserted at the specified location.
+        Otherwise, the file content is decompressed (if possible) and copied \
+        into the destination file.
+
+        :param dest: destination file path or writable file-like object
+        :param args: additional arguments for the decompression
+        :param kwargs: additional arguments for the decompression
+        """
+    @overload
+    def extract(self, dest: None = None, *args, **kwargs) -> BytesIO :
+        """Extract the content of the entry and store it in a `BytesIO` object
+
+        The file content is decompressed (if possible) and copied \
+        into the created `BytesIO` object.
+
+        :param args: additional arguments for the decompression
+        :param kwargs: additional arguments for the decompression
+        """
+
+    def extract(self, dest: str | BytesRW | None = None, *args, **kwargs) :
+        if self._name is not None :
+            ext = self._name[self._name.rindex('.')+1:]
+            if isinstance(dest, str) and ext == dest[dest.rfind('.')+1:] :
+                self.to_file(dest)
+                return
+        else :
+            ext = None
+        match ext :
+            case None | 'mp4' | 'chs' | 'ccit' | 'hw' :
+                return self.to_file(dest)
+            case 'cbg' :
+                cbg = CompressedBG(self.data)
+                return cbg.img_write(dest, *args, **kwargs)
+            case 'ctd' :
+                data = self.data
+                lenzu_header = data[:16]
+                if lenzu_header == b"LenZuCompressor\0" :
+                    return lenzu_decompress(BytesIO(data), len(data), dest)
+                else :
+                    return self.to_file(dest)
+            case 'mzp' :
+                mzpImg = MzpImage(self.data)
+                return mzpImg.img_write(dest)
+            case _ :
+                raise ValueError(f'Unimplemented extraction of {self._name}')
+
+    def inject(self, src: str | BytesReader | bytes, **kwargs) :
+        """Inject a new content to the entry
+        
+        If the source is a file path that has the same extension as the entry, \
+        the raw bytes are read from the file and inserted in the entry.
+        Otherwise, the source file content is compressed (if possible) and \
+        copied to the entry.
+        """
+        if self._name is not None :
+            ext = self._name[self._name.rindex('.')+1:]
+            if isinstance(src, str) and src[src.rfind('.')+1:] == ext :
+                self.from_file(src)
+                return
+        else :
+            ext = None
+        match ext :
+            case None | 'ctd' | 'mp4' | 'chs' | 'ccit' | 'hw' :
+                match src :
+                    case str() :
+                        self.from_file(src)
+                    case bytes() :
+                        self.data = src
+                    case bytearray() | memoryview() :
+                        self.data = bytes(src)
+                    case _ :
+                        self.from_file(src)
+            case 'cbg' :
+                CompressedBG(self.data).img_read(src)
+            case 'mzp' :
+                mzpImg = MzpImage(self.data)
+                mzpImg.img_read(src, **kwargs)
+                self.from_file(mzpImg.mzp_write())
+            case _ :
+                raise ValueError(f'Unimplemented injection to {self._name}')
+
+#endregion #####################################################################
+#region                            MRG archive
+################################################################################
 
 class MrgArchive :
 
